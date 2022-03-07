c0d0138e8de1b5bd7e1e01fa6de115e0eb58fcce
Omar <omaar_5@hotmail.com>
2022-03-07 19:17:47 +0200

    fix console errors

--- global/NextHead.js
+++ global/NextHead.js
@@ -14,7 +14,7 @@ export const NextHead = ({ title }) => {
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
-        crossOrigin={true}
+        crossOrigin="true"
       />
 
       {/* Links: Style-Sheets */}
