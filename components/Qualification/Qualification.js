eeeba537ff84958b9012ef57740df33dec089890
Omar <omaar_5@hotmail.com>
2022-03-11 16:46:41 +0200

    complete Qualification section responsive

--- components/Qualification/Qualification.js
+++ components/Qualification/Qualification.js
@@ -1,12 +1,64 @@
-import { QualificationContainer } from "./styledQualification";
-import { Section, Container } from "../../global/GlobalStyle";
+import {
+  QualificationContainer,
+  QualificationNav,
+  QualificationTab,
+  QualificationDetails,
+  QualificationTitle,
+  QualificationSubTitle,
+  QualificationDate,
+} from "./styledQualification";
+import { Section, Container, SectionTitle } from "../../global/GlobalStyle";
+import { useState } from "react";
 
-export const Qualification = () => {
+export const Qualification = ({ data }) => {
+  const [activeTab, setActiveTab] = useState("Education");
+
+  const educationTab = data.education.map((education) => (
+    <QualificationDetails key={education.id}>
+      <QualificationTitle>{education.title}</QualificationTitle>
+      <QualificationSubTitle>{education.subtitle}</QualificationSubTitle>
+      <QualificationDate>
+        <strong>{education.start}</strong> - <strong>{education.finish}</strong>
+      </QualificationDate>
+    </QualificationDetails>
+  ));
+
+  const workTab = data.work.map((work) => (
+    <QualificationDetails key={work.id}>
+      <QualificationTitle>{work.title}</QualificationTitle>
+      <QualificationSubTitle>{work.subtitle}</QualificationSubTitle>
+      <QualificationDate>
+        <strong>{work.start}</strong> - <strong>{work.finish}</strong>
+      </QualificationDate>
+    </QualificationDetails>
+  ));
+
   return (
-    <Section id="qualification">
+    <Section id="qualification" image={true} url={data.sectionBg}>
       <Container>
+        <SectionTitle style={{ color: "#fff" }}>My Qualifications</SectionTitle>
         <QualificationContainer>
-          <h1>TODO: Qualification</h1>
+          <QualificationNav>
+            <p
+              className={`${activeTab === "Education" ? "active" : ""}`}
+              onClick={() => setActiveTab("Education")}
+            >
+              Education
+            </p>
+            <p
+              className={`${activeTab === "Work" ? "active" : ""}`}
+              onClick={() => setActiveTab("Work")}
+            >
+              Work
+            </p>
+          </QualificationNav>
+          <QualificationTab>
+            {activeTab === "Education"
+              ? educationTab
+              : activeTab === "Work"
+              ? workTab
+              : ""}
+          </QualificationTab>
         </QualificationContainer>
       </Container>
     </Section>
--- components/Qualification/styledQualification.js
+++ components/Qualification/styledQualification.js
@@ -1,3 +1,71 @@
 import styled from "styled-components";
 
-export const QualificationContainer = styled.div``;
+export const QualificationContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.75);
+  padding: 1rem;
+  border-radius: 1rem;
+  color: #fff;
+  transform: translateY(20%);
+
+  @media screen and (max-width: 568px) {
+    transform: translateY(0);
+  }
+`;
+
+export const QualificationNav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0 5rem;
+
+  p {
+    font-weight: 700;
+    font-size: ${({ theme }) => theme.typo.smallTitleFont};
+    cursor: pointer;
+    border-bottom: 5px solid transparent;
+    transition: all 0.5s ease-in;
+
+    &.active {
+      border-bottom: 5px solid ${({ theme }) => theme.lightColors.primaryColor};
+      color: ${({ theme }) => theme.lightColors.primaryColor};
+    }
+
+    :hover {
+      border-bottom: 5px solid ${({ theme }) => theme.lightColors.primaryColor};
+      color: ${({ theme }) => theme.lightColors.primaryColor};
+    }
+  }
+`;
+
+export const QualificationTab = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1.5rem;
+
+  @media screen and (max-width: 568px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export const QualificationDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem 0;
+  margin: 0 auto;
+  width: 250px;
+  height: 150px;
+`;
+
+export const QualificationTitle = styled.p`
+  margin: 0;
+  color: ${({ theme }) => theme.lightColors.primaryColor};
+  font-size: ${({ theme }) => theme.typo.menuLinkFont};
+`;
+
+export const QualificationSubTitle = styled.p`
+  margin: 0;
+`;
+
+export const QualificationDate = styled.p`
+  margin: 0;
+`;
