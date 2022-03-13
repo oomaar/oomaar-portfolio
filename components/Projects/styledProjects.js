c8749130166f31da096d013de1e8d7a93cd34b3e
Omar <omaar_5@hotmail.com>
2022-03-13 21:09:30 +0200

    setup Carousel

--- components/Projects/styledProjects.js
+++ components/Projects/styledProjects.js
@@ -1,3 +1,48 @@
 import styled from "styled-components";
 
 export const ProjectsContainer = styled.div``;
+
+export const CarouselContainer = styled.div`
+  position: relative;
+  overflow: hidden;
+  height: 615px;
+  border: 1px solid red;
+  display: flex;
+`;
+
+export const CarouselSlide = styled.div`
+  overflow: hidden;
+  width: 0;
+  opacity: 0;
+  visibility: hidden;
+  transition: all 0.5s ease-in;
+
+  &.active {
+    width: 100%;
+    opacity: 1;
+    visibility: visible;
+  }
+`;
+
+export const Arrows = styled.div`
+  position: absolute;
+  top: 20%;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 0.5rem;
+`;
+
+export const Arrow = styled.div`
+  font-size: 2rem;
+  cursor: pointer;
+  transition: all 0.5s ease-in;
+  border: 1px solid transparent;
+  border-radius: 0.7rem;
+  padding: 1rem;
+
+  :hover {
+    color: ${({ theme }) => theme.lightColors.primaryColor};
+  }
+`;
