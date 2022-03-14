26e316ef3d2f69bc3a0d1b719fab3d343a68a7c5
Omar <omaar_5@hotmail.com>
2022-03-14 22:54:05 +0200

    destroy old carousel

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -9,42 +9,13 @@ import {
 } from "./styledProjects";
 
 export const Projects = () => {
-  const carouselImages = [
-    {
-      id: 1,
-      title: "TODO: Projects 1",
-    },
-    {
-      id: 2,
-      title: "TODO: Projects 2",
-    },
-  ];
-  const len = carouselImages.length - 2;
-  const [activeIndex, setActiveIndex] = useState(1);
-
-  // Arrows
-  const prevSlide = () =>
-    setActiveIndex(activeIndex < 1 ? len : activeIndex - 1);
-  const nextSlide = () =>
-    setActiveIndex(activeIndex === len ? 0 : activeIndex + 1);
-
   return (
     <Section id="projects">
       <Container>
         <ProjectsContainer>
           <CarouselContainer>
-            {carouselImages.map((test, index) => (
-              <CarouselSlide
-                key={index}
-                className={`${test.id === activeIndex ? "active" : ""}`}
-              >
-                <h1>{test.title}</h1>
-              </CarouselSlide>
-            ))}
-            <Arrows>
-              <Arrow onClick={prevSlide}>&#10094;</Arrow>
-              <Arrow onClick={nextSlide}>&#10095;</Arrow>
-            </Arrows>
+            {/* <Arrow onClick={prevSlide}>&#10094;</Arrow> */}
+            {/* <Arrow onClick={nextSlide}>&#10095;</Arrow> */}
           </CarouselContainer>
         </ProjectsContainer>
       </Container>
--- components/Projects/styledProjects.js
+++ components/Projects/styledProjects.js
@@ -2,47 +2,10 @@ import styled from "styled-components";
 
 export const ProjectsContainer = styled.div``;
 
-export const CarouselContainer = styled.div`
-  position: relative;
-  overflow: hidden;
-  height: 615px;
-  border: 1px solid red;
-  display: flex;
-`;
+export const CarouselContainer = styled.div``;
 
-export const CarouselSlide = styled.div`
-  overflow: hidden;
-  width: 0;
-  opacity: 0;
-  visibility: hidden;
-  transition: all 0.5s ease-in;
+export const CarouselSlide = styled.div``;
 
-  &.active {
-    width: 100%;
-    opacity: 1;
-    visibility: visible;
-  }
-`;
+export const Arrows = styled.div``;
 
-export const Arrows = styled.div`
-  position: absolute;
-  top: 20%;
-  left: 0;
-  right: 0;
-  display: flex;
-  justify-content: space-between;
-  padding: 0 0.5rem;
-`;
-
-export const Arrow = styled.div`
-  font-size: 2rem;
-  cursor: pointer;
-  transition: all 0.5s ease-in;
-  border: 1px solid transparent;
-  border-radius: 0.7rem;
-  padding: 1rem;
-
-  :hover {
-    color: ${({ theme }) => theme.lightColors.primaryColor};
-  }
-`;
+export const Arrow = styled.div``;
