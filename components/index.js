aa6a98541695f3b47d48f6b0f61220f9fcf013ea
Omar <omaar_5@hotmail.com>
2022-03-21 21:46:42 +0200

    add HeroAnimation Component

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { Hero } from "./Hero/Hero";
+export { HeroAnimation } from "./Hero/HeroAnimation";
 export { SocialMedia } from "./SocialMedia/SocialMedia";
 export { About } from "./About/About";
 export { Qualification } from "./Qualification/Qualification";
