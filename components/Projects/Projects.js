dce3a48d0cf23380fc72f233055f7407909965de
Omar <omaar_5@hotmail.com>
2022-03-23 20:17:29 +0200

    projects new HTML

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,18 +1,88 @@
+import { useState } from "react";
 import {
   Container,
   Section,
   SectionSubTitle,
   SectionTitle,
 } from "../../global/GlobalStyle";
-import { ProjectsContainer } from "./styledProjects";
+import {
+  ProjectsContainer,
+  ProjectsNav,
+  ProjectsNavLink,
+  ProjectsCard,
+  ProjectsCardTitle,
+  ProjectsCardButton,
+} from "./styledProjects";
 
 export const Projects = ({ data }) => {
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  const workProjects = data.work.map((work) => (
+    <ProjectsCard>
+      <img src={work.src} alt={work.alt} />
+      <ProjectsCardTitle>{work.title}</ProjectsCardTitle>
+      <ProjectsCardButton href={work.url}>
+        Demo <i className="bx bx-right-arrow-alt"></i>
+      </ProjectsCardButton>
+    </ProjectsCard>
+  ));
+
+  const hobbyProjects = data.hobby.map((hobby) => (
+    <ProjectsCard>
+      <img src={hobby.src} alt={hobby.alt} />
+      <ProjectsCardTitle>{hobby.title}</ProjectsCardTitle>
+      <ProjectsCardButton href={hobby.url}>
+        Demo <i className="bx bx-right-arrow-alt"></i>
+      </ProjectsCardButton>
+    </ProjectsCard>
+  ));
+
+  const clonesProjects = data.clones.map((clones) => (
+    <ProjectsCard>
+      <img src={clones.src} alt={clones.alt} />
+      <ProjectsCardTitle>{clones.title}</ProjectsCardTitle>
+      <ProjectsCardButton href={clones.url}>
+        Demo <i className="bx bx-right-arrow-alt"></i>
+      </ProjectsCardButton>
+    </ProjectsCard>
+  ));
+
   return (
     <Section id="projects">
       <SectionSubTitle>My Portfolio</SectionSubTitle>
       <SectionTitle>Recent Projects</SectionTitle>
+      <ProjectsNav>
+        <ProjectsNavLink onClick={() => setActiveCategory("All")}>
+          All
+        </ProjectsNavLink>
+        <ProjectsNavLink onClick={() => setActiveCategory("Work")}>
+          Work
+        </ProjectsNavLink>
+        <ProjectsNavLink onClick={() => setActiveCategory("Hobby")}>
+          Hobby
+        </ProjectsNavLink>
+        <ProjectsNavLink onClick={() => setActiveCategory("Clones")}>
+          Clones
+        </ProjectsNavLink>
+      </ProjectsNav>
       <Container>
-        <ProjectsContainer></ProjectsContainer>
+        <ProjectsContainer>
+          {activeCategory === "All" ? (
+            <>
+              {workProjects}
+              {hobbyProjects}
+              {clonesProjects}
+            </>
+          ) : activeCategory === "Work" ? (
+            <>{workProjects}</>
+          ) : activeCategory === "Hobby" ? (
+            <>{hobbyProjects}</>
+          ) : activeCategory === "Clones" ? (
+            <>{clonesProjects}</>
+          ) : (
+            <></>
+          )}
+        </ProjectsContainer>
       </Container>
     </Section>
   );
--- components/Projects/styledProjects.js
+++ components/Projects/styledProjects.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
-export const ProjectsContainer = styled.div``;
-
 export const ProjectsNav = styled.nav``;
 
 export const ProjectsNavLink = styled.span``;
+
+export const ProjectsContainer = styled.div`
+  display: grid;
+  gap: 1.5rem;
+`;
+
+export const ProjectsCard = styled.div`
+  img {
+  }
+`;
+
+export const ProjectsCardTitle = styled.h3``;
+
+export const ProjectsCardButton = styled.a``;
