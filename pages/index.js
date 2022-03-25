2ee8c7c8820ef070e92446d5dcb9e4ea656dbf73
Omar <omaar_5@hotmail.com>
2022-03-25 17:33:30 +0200

    refactor index page

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,20 @@
-import { useState } from "react";
+import { NextHead } from "../global/NextHead";
+import portfolioData from "../data/portfolioData.json";
+import { Main } from "../global/GlobalStyle";
 import {
   About,
   Contact,
   Footer,
   Hero,
-  Navbar,
   Projects,
   Qualification,
-  Sidebar,
   Skills,
-  SocialMedia,
 } from "../components";
-import { NextHead } from "../global/NextHead";
-import portfolioData from "../data/portfolioData.json";
-import { Main } from "../global/GlobalStyle";
 
 export default function Home() {
-  const [showMenu, setShowMenu] = useState(false);
-
   return (
     <div>
       <NextHead title="Omar's Resume" />
-
-      <Navbar
-        data={portfolioData.navbar}
-        showMenu={showMenu}
-        setShowMenu={setShowMenu}
-      />
-      {/* <Sidebar
-        data={portfolioData.sidebar}
-        showMenu={showMenu}
-        setShowMenu={setShowMenu}
-      /> */}
-      {/* <SocialMedia data={portfolioData.socialMedia} /> */}
       <Main>
         <Hero data={portfolioData.hero} />
         <About data={portfolioData.about} />
