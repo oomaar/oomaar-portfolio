126254a72b1f3dd03374694e864909a0c1efda70
Omar <omaar_5@hotmail.com>
2022-03-25 16:56:54 +0200

    darkTheme function

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, lightTheme, theme } from "../global/GlobalStyle";
 
 export default function App({ Component, pageProps }) {
+  const [toggleTheme, setToggleTheme] = useState("");
+
+  const checkTheme = () => {
+    if (toggleTheme === "light") {
+      setToggleTheme("dark");
+      localStorage.setItem("omars-dark-value", "dark");
+    } else if (toggleTheme === "dark") {
+      setToggleTheme("light");
+      localStorage.setItem("omars-dark-value", "light");
+    }
+  };
+
+  useEffect(() => {
+    const themeValue = localStorage.getItem("omars-dark-value");
+    setToggleTheme(themeValue);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <ThemeProvider theme={lightTheme}>
